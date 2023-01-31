82afd8e0c82f8f190843e581a7d58f8e905c934e
shailx383 <65397549+shailx383@users.noreply.github.com>
2023-01-31 18:55:44 +0530

    fit function first draft

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+
 class NaiveBayesClassifier:
     def __init__(self, X, y):
         self.classes = list(y.unique())
@@ -64,12 +65,13 @@ class NaiveBayesClassifier:
 
 
 class Node:
-    def __init__(self, feature, impurity, thresh, left_node, right_node, isLeaf):
+    def __init__(self, feature=None, impurity=None, thresh=None, left_node=None, right_node=None, value=None, isLeaf=False):
         self.feature = feature
         self.impurity = impurity
         self.thresh = thresh
         self.left_node = left_node
         self.right_node = right_node
+        self.value = value
         self.isLeaf = isLeaf
 
 
@@ -81,13 +83,67 @@ class DecisionTree:
     
     def fit(self, X, y):
         df = pd.concat([X, y], axis = 1)
-        self.root = self.make_tree(df)
+        self.root = self.make_tree(0, df)
     
-    # def make_tree(self, depth, df):
-    #     rows = len(df.iloc[:,:-1])
-    #     features = list(df.iloc[:,:-1].columns)
-    #     if self.max_depth > depth and rows > self.min_rows_split:
+    def make_tree(self, depth, df):
+        rows = len(df.iloc[:,:-1])
+        features = list(df.iloc[:,:-1].columns)
+        if self.max_depth > depth and rows > self.min_rows_split:
+            best_split = self.optimal_feature(df)
+            if best_split['gini'] > 0:
+                l_tree = self.make_tree(depth+1, best_split['left_split'])
+                r_tree = self.make_tree(depth+1, best_split['right_split'])
+                return Node(best_split['feat'], best_split['gini'], best_split['thresh'], l_tree, r_tree)
+        final_value = self.final_label(df.iloc[:,-1])
+        return Node(value=final_value, isLeaf=True)
 
-    def optimal_feature(self, df, )
+
+    def optimal_feature(self, df):
+        X = df.iloc[:,:-1]
+        y = df.iloc[:,-1]
+        features = list(X.columns)
+        total_rows = len(X)
+        min_gini = float("inf")
+        for feature in features:
+            values = df[feature].unique()
+            for val in values:
+                l_condition = df[feature] <= val
+                r_condition = df[feature] > val
+                l_df = df[l_condition]
+                r_df = df[r_condition]
+                if len(l_df) > 0 and len(r_df) > 0:
+                    gini = self.weighted_gini_impurity(l_df, r_df)
+                    if gini < min_gini:
+                        min_gini = gini
+                        result = {
+                            'feat': feature,
+                            'thresh': val,
+                            'left_split': l_df,
+                            'right_split': r_df,
+                            'gini': gini
+                        }
+        return result
+
+    def weighted_gini_impurity(self, l, r):
+        weight_r = len(r)/(len(l)+len(r))
+        weight_l = 1 - weight_r
+        return (weight_r*(self.gini_impurity(r)) + weight_l*(self.gini_impurity(l)))
 
 
+    def gini_impurity(self, subset):
+        target  = subset.iloc[:,-1]
+        sub_label_distr = dict(target.value_counts())
+        total = len(target)
+        sum_squares = 0
+        for label in sub_label_distr.keys():
+            sum_squares += (sub_label_distr[label]/total)**2
+        return 1 - sum_squares
+    
+    def final_label(self, col):
+        return (dict(map(reversed, dict(col.value_counts()).items()))[max(dict(col.value_counts()).values())])
+
+    def predict(self, input, tree):
+        if tree.isLeaf:
+            return tree.value
+        else:
+            return self.predict()
